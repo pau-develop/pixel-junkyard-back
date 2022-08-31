c7dd7a109d657afd8c65f0f4e33d466b848dca67
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-31 11:49:52 +0200

    Tested hashCompare function

--- src/utils/auth.test.ts
+++ src/utils/auth.test.ts
@@ -1,4 +1,4 @@
-import { hashCreator } from "./auth";
+import { hashCompare, hashCreator } from "./auth";
 
 describe("Given a hasCreate function", () => {
   describe("When instantiated with a string as an argument", () => {
@@ -10,6 +10,27 @@ describe("Given a hasCreate function", () => {
 
       expect(result.startsWith(hashStart)).toBe(true);
       expect(result.length > 10).toBe(true);
+    });
+  });
+});
+
+describe("Given a hasCompare function", () => {
+  describe("When called a password and a string", () => {
+    test("Then it should return false if the password doesn't match the second string", async () => {
+      const password = "admin";
+
+      const result = await hashCompare(password, password);
+
+      expect(result).toBe(false);
+    });
+
+    test("Then it should return true if the password matches the second string, being it the correct hash", async () => {
+      const password = "admin";
+      const hash = await hashCreator(password);
+
+      const result = await hashCompare(password, hash);
+
+      expect(result).toBe(true);
     });
   });
 });
