07b496fa86e743b1525dd2a2d8056f50b6a42dcf
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-31 11:34:43 +0200

    Created createToken function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,13 @@
 import "../loadEnvironment";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const hashCreator = (text: string) => {
   const salt = 10;
   return bcrypt.hash(text, salt);
 };
+
+export const createToken = (payload: JwtPayload) =>
+  jwt.sign(payload, process.env.SECRET);
 
 export default hashCreator;
