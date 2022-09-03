35b4981f922577c7b95127e96b0622639c703273
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-03 21:21:24 +0200

    Created getUserById controller

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllUsers } from "../controllers/usersControllers";
+import { getAllUsers, getUserById } from "../controllers/usersControllers";
 import tokenVerification from "../middlewares/tokenVerification";
 
 const usersRouter = express.Router();
 
 usersRouter.get("/all", tokenVerification, getAllUsers);
+usersRouter.get("/:id", tokenVerification, getUserById);
 
 export default usersRouter;
