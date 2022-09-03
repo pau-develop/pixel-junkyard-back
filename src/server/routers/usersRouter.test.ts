a8916f1b8294fe701e8cd7344e8de27e092e137c
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-03 18:47:33 +0200

    Updated usersRouter test with auth header

--- src/server/routers/usersRouter.test.ts
+++ src/server/routers/usersRouter.test.ts
@@ -27,7 +27,11 @@ afterAll(async () => {
 describe("Given a usersRouter", () => {
   describe("When it receives a request on /all path", () => {
     test("It should call the getAllUsers controller function and return an array of users and the status 200", async () => {
-      const { body } = await request(app).get("/users/all").expect(200);
+      const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEzODEyYmJmNzVkMGRkYzkzNjBjY2UiLCJ1c2VyTmFtZSI6InRlc3RpbmdUb2tlbiIsImlhdCI6MTY2MjIyMjY0OX0.dBZVoz75C6Uw-Nakfa0QtMcU-2nQc_Msko3YZc87k6c`;
+      const { body } = await request(app)
+        .get("/users/all")
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200);
 
       expect(body).toHaveProperty("users", []);
     });
