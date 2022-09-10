e176b5db4ac90a7d2bc25e8e1a30951acdf95c82
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-10 02:52:06 +0200

    Using bodyParser to extend file size

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import userRouter from "./routers/userRouter";
 import usersRouter from "./routers/usersRouter";
@@ -12,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-
+app.use(bodyParser.json({ limit: "5mb" }));
 app.use("/user", userRouter);
 app.use("/users", usersRouter);
 app.use("/drawings", drawingsRouter);
