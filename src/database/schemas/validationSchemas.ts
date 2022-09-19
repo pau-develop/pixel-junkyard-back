3909a3f6fd6e655bb97f975035c92d88e06f7e7d
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-19 21:44:13 +0200

    Changing Joi validation requirements for email

--- src/database/schemas/validationSchemas.ts
+++ src/database/schemas/validationSchemas.ts
@@ -3,7 +3,7 @@ import { Joi } from "express-validation";
 export const registerSchema = Joi.object({
   userName: Joi.string().min(3).max(12).required(),
   password: Joi.string().min(5).max(20).required(),
-  email: Joi.string().min(10).max(25).required(),
+  email: Joi.string().min(5),
   avatar: Joi.string().min(3),
 });
 
